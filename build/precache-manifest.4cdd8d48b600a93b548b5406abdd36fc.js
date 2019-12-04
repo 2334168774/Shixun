b295228da9308cabe13d5657fed3bcfffa82d704
2334168774 <2334168774@qq.com>
2019-12-04 21:29:25 +0800

    update

---
+++ build/precache-manifest.4cdd8d48b600a93b548b5406abdd36fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108678ff8ef0acdbfdbf39e542943260",
+    "url": "index.html"
+  },
+  {
+    "revision": "280a57c58c67759385d7",
+    "url": "static/css/2.59c644fd.chunk.css"
+  },
+  {
+    "revision": "9b0c1fd5779cc741bb83",
+    "url": "static/css/main.306571c7.chunk.css"
+  },
+  {
+    "revision": "280a57c58c67759385d7",
+    "url": "static/js/2.a8d5323e.chunk.js"
+  },
+  {
+    "revision": "9b0c1fd5779cc741bb83",
+    "url": "static/js/main.e6544511.chunk.js"
+  },
+  {
+    "revision": "067790caabdba6a1564e",
+    "url": "static/js/runtime-main.d10b7575.js"
+  },
+  {
+    "revision": "6d04b8794c1726337e6f2b872cb882cd",
+    "url": "static/media/2.6d04b879.jpg"
+  },
+  {
+    "revision": "d672ff82a4cf7c886ca4eadcd11bc9d2",
+    "url": "static/media/xx.d672ff82.jpg"
+  }
+]);
